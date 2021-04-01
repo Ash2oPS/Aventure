0aa56fa83c3a1048d97d3250f0dae57709c8f542
Etienne Sibeaux <esibeaux.pro@outlook.fr>
2021-04-01 11:45:37 +0200

    Le Debut d'une longue quete sympathique

--- A_Kid_in_Space_2.js
+++ A_Kid_in_Space_2.js
@@ -26,12 +26,21 @@ const config = {
 
 ////////// VARIABLES //////////
 
+// -- Config --
+
 var game = new Phaser.Game(config);
 
-// -- Acteurs Vivants --
+var cursors;
 
-var maya
-var maya2
+// -- Joueur --
+
+var joueur;
+var joueurWalkspeed;
+
+
+
+
+
 
 
 
@@ -41,27 +50,98 @@ function preload(){
 
     // -- Acteurs Vivants --
 
-    this.load.spritesheet('maya', 'assets/maya/MayaBase1.png', {frameWidth : 64, frameHeight : 64});
-    this.load.spritesheet('maya2', 'assets/maya/MayaBase2.png', {frameWidth : 128, frameHeight : 128});
+    this.load.spritesheet('joueur', 'assets/maya/MayaBase2.png', {frameWidth : 128, frameHeight : 128});
 
 }
+
+
+
+
 
 ////////// CREATE //////////
 
 function create(){
 
-    // -- Acteurs Vivants --
+    // -- Inputs --
 
-    maya = this.physics.add.sprite(400, 400, 'maya');
-    maya2 = this.physics.add.sprite(600, 600, 'maya2');
+    cursors = this.input.keyboard.addKeys(
+        {up:Phaser.Input.Keyboard.KeyCodes.Z,
+        down:Phaser.Input.Keyboard.KeyCodes.S,
+        left:Phaser.Input.Keyboard.KeyCodes.Q,
+        right:Phaser.Input.Keyboard.KeyCodes.D});
+
+
+    // Joueur
+
+    joueur = this.physics.add.sprite(600, 600, 'joueur');
+    joueurWalkspeed = 3;
 
 
 }
+
+
+
+
 
 ////////// UPDATE //////////
 
 function update(){
 
+    deplacementsJoueur();                                   //Le joueur se déplace (ZQSD/LStick)
 
+
+}
+
+
+
+
+
+////////// FUNCTIONS //////////
+
+function deplacementsJoueur(){
+
+
+    // -- Diagonales (anti straff acceleration (selon le théorème de Pythagore)) --
+
+
+    if (cursors.right.isDown && cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown){
+        joueur.x += joueurWalkspeed/Math.sqrt(2);                       
+        joueur.y -= joueurWalkspeed/Math.sqrt(2); 
+    }
+
+    else if (cursors.left.isDown && cursors.up.isDown && !cursors.down.isDown && !cursors.right.isDown){
+        joueur.x -= joueurWalkspeed/Math.sqrt(2);                      
+        joueur.y -= joueurWalkspeed/Math.sqrt(2);                       
+    }
+
+    else if (cursors.down.isDown && cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){
+        joueur.x -= joueurWalkspeed/Math.sqrt(2);                      
+        joueur.y += joueurWalkspeed/Math.sqrt(2);                       
+    }
+
+    else if (cursors.down.isDown && cursors.right.isDown && !cursors.left.isDown && !cursors.up.isDown){
+        joueur.x += joueurWalkspeed/Math.sqrt(2);                      
+        joueur.y += joueurWalkspeed/Math.sqrt(2);                         
+    }
+
+    // -- Déplacements Horizontaux Verticaux --
+
+
+    else if (cursors.right.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.up.isDown){
+        joueur.x += joueurWalkspeed;                        // Déplacement vers la droite
+    }
+
+    else if (cursors.left.isDown && !cursors.right.isDown && !cursors.down.isDown && !cursors.up.isDown){
+        joueur.x -= joueurWalkspeed;                        // Déplacement vers la gauche
+    }
+
+    else if (cursors.down.isDown && !cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){
+        joueur.y += joueurWalkspeed;                        // Déplacement vers le bas
+    }
+
+    else if (cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.right.isDown){
+        joueur.y -= joueurWalkspeed;                        // Déplacement vers le haut
+    }
+    
 
 }
