27b04d5a064d71590f67b7fcc4bbf3b2acb9a2bd
Etienne Sibeaux <esibeaux.pro@outlook.fr>
2021-04-20 19:24:57 +0200

    Allez, hop on nettoie un peu tout ça !

--- A_Crocodile_In_A_Mystery_Dungeon.js
+++ A_Crocodile_In_A_Mystery_Dungeon.js
@@ -39,17 +39,17 @@ var cursors;
 
 // -- player --
 
-var player;             //gameObjects
+var player; //gameObjects
 var shadow;
 
-var playerWalkspeed;    //mechanical stats
+var playerWalkspeed; //mechanical stats
 var playerIsMoving;
 var currentX;
 var nextX;
 var currentY;
 var nextY;
 
-var playerMoney;        //explicit stats
+var playerMoney; //explicit stats
 var playerKey;
 var playerLightning;
 var playerFire;
@@ -75,7 +75,7 @@ function preload() {
     // -- Acteurs Vivants --
 
     this.load.spritesheet('player', 'assets/player/gator.png', {
-        frameWidth: 100,
+        frameWidth : 100,
         frameHeight: 100
     });
     this.load.image('shadow', 'assets/player/shadow.png');
@@ -96,60 +96,28 @@ function preload() {
 function create() {
 
     // -- Debugtext --
-    
+
     initDebug(this);
 
     // -- UI --
 
     initUi(this);
 
-
     // -- Tiled --
 
-    const map = this.make.tilemap({
-        key: 'map'
-    });
-    const tileset = map.addTilesetImage('Tileset', 'tiles');
-
-    var ground_Layer = map.createLayer('Ground', tileset);
-    var wall_Layer = map.createLayer('Wall', tileset);
-
+    initTiled(this);
 
     // -- Inputs --
 
-    cursors  = this.input.keyboard.addKeys({
-        up   : Phaser.Input.Keyboard.KeyCodes.Z,
-        down : Phaser.Input.Keyboard.KeyCodes.S,
-        left : Phaser.Input.Keyboard.KeyCodes.Q,
-        right: Phaser.Input.Keyboard.KeyCodes.D
-    });
-
+    initInputs(this);
 
     // -- player --
 
-    shadow          = this.add.image(750, 490, 'shadow')
-        .setOrigin(.5, 1)
-        .setDepth(1);
-    player          = this.physics.add.sprite(750, 475, 'player')
-        .setOrigin(.5, 1)
-        .setDepth(1);
-    playerWalkspeed = 5;
-    playerIsMoving  = false;
-    currentX        = 0;
-    nextX           = 0;
-    currentY        = 0;
-    nextY           = 0;
-    playerMoney     = 150;
-    playerKey       = 1;
-    playerLightning = 1;
-    playerFire      = 1;
-    currentFloor    = 0;
-
+    initPlayer(this);
 
     // -- Camera --
 
-    this.cameras.main.startFollow(player);
-    this.cameras.main.setBounds(0, 0, player.widthInPixels, player.heightInPixels);
+    initCamera(this);
 
 
 }
@@ -175,164 +143,212 @@ function update() {
 
 ////////// FUNCTIONS //////////
 
-function initDebug(context){
+function initDebug(context) {
     if (config.physics.arcade.debug) {
-        debugText          = context.add.text(0, 1080, "bonjour, ça va ? super", {
-            fontSize       : '24px',
-            padding        : {
-                x          : 10,
-                y          : 5
+        debugText = context.add.text(0, 1080, "bonjour, ça va ? super", {
+            fontSize: '24px',
+            padding: {
+                x: 10,
+                y: 5
             },
             backgroundColor: '#000000',
-            fill           : '#ffffff'
+            fill: '#ffffff'
         });
         debugText.setScrollFactor(0)
             .setOrigin(0, 1)
             .setDepth(11);
 
-        printCases(context, 2, 1, 12, 7);              // Affiche les coordonnées des cases entre [departX;departY] et [arriveeX;arriveeY]
+        printCases(context, 2, 1, 12, 7); // Affiche les coordonnées des cases entre [departX;departY] et [arriveeX;arriveeY]
     }
-}           //What's in the Create Event and that's about the custom debugger
+} //What's in the Create Event and that's about the custom debugger
 
 
-function initUi(context){
+function initUi(context) {
     uiTextKey = context.add.text(106, 106, playerKey, {
-        fontSize : '24px',
-        padding : {
-            x : 10,
-            y : 10
-        },
-    })
-    .setScrollFactor(0)
-    .setOrigin(1,1)
-    .setDepth(11);
+            fontSize: '24px',
+            padding: {
+                x: 10,
+                y: 10
+            },
+        })
+        .setScrollFactor(0)
+        .setOrigin(1, 1)
+        .setDepth(11);
 
     uiTextMoney = context.add.text(106, 202, playerMoney, {
-        fontSize : '24px',
-        padding : {
-            x : 10,
-            y : 10
-        }
-    })
-    .setScrollFactor(0)
-    .setOrigin(1,1)
-    .setDepth(11);
+            fontSize: '24px',
+            padding: {
+                x: 10,
+                y: 10
+            }
+        })
+        .setScrollFactor(0)
+        .setOrigin(1, 1)
+        .setDepth(11);
 
     uiTextFire = context.add.text(106, 318, playerFire, {
-        fontSize : '24px',
-        padding : {
-            x : 10,
-            y : 10
-        }
-    })
-    .setScrollFactor(0)
-    .setOrigin(1,1)
-    .setDepth(11);
+            fontSize: '24px',
+            padding: {
+                x: 10,
+                y: 10
+            }
+        })
+        .setScrollFactor(0)
+        .setOrigin(1, 1)
+        .setDepth(11);
 
     uiTextLightning = context.add.text(106, 414, playerLightning, {
-        fontSize : '24px',
-        padding : {
-            x : 10,
-            y : 10
-        }
-    })
-    .setScrollFactor(0)
-    .setOrigin(1,1)
-    .setDepth(11);
+            fontSize: '24px',
+            padding: {
+                x: 10,
+                y: 10
+            }
+        })
+        .setScrollFactor(0)
+        .setOrigin(1, 1)
+        .setDepth(11);
 
     uiTextStair = context.add.text(1910, 114, playerLightning, {
-        fontSize : '26px',
-        fill     : '#F5AEB2'
-    })
-    .setScrollFactor(0)
-    .setOrigin(1,1)
-    .setDepth(11);
+            fontSize: '26px',
+            fill: '#F5AEB2'
+        })
+        .setScrollFactor(0)
+        .setOrigin(1, 1)
+        .setDepth(11);
 
 
 
     context.add.image(10, 10, 'keyIcon')
-    .setDepth(10)
-    .setScrollFactor(0)
-    .setOrigin(0,0);
+        .setDepth(10)
+        .setScrollFactor(0)
+        .setOrigin(0, 0);
 
     context.add.image(10, 106, 'moneyIcon')
-    .setDepth(10)
-    .setScrollFactor(0)
-    .setOrigin(0,0);
+        .setDepth(10)
+        .setScrollFactor(0)
+        .setOrigin(0, 0);
 
     context.add.image(10, 222, 'fireIcon')
-    .setDepth(10)
-    .setScrollFactor(0)
-    .setOrigin(0,0);
+        .setDepth(10)
+        .setScrollFactor(0)
+        .setOrigin(0, 0);
 
     context.add.image(10, 318, 'lightningIcon')
-    .setDepth(10)
-    .setScrollFactor(0)
-    .setOrigin(0,0);
+        .setDepth(10)
+        .setScrollFactor(0)
+        .setOrigin(0, 0);
 
     context.add.image(1910, 10, 'stairIcon')
-    .setDepth(10)
-    .setScrollFactor(0)
-    .setOrigin(1,0);
-}           //What's in the Create Event and that's about UI
+        .setDepth(10)
+        .setScrollFactor(0)
+        .setOrigin(1, 0);
+} //What's in the Create Event and that's about UI
 
 
-function printCases(context, departX, departY, arriveeX, arriveeY){
-    for (i = departX; i < arriveeX + 1; i++){
-        for (j = departY; j < arriveeY + 1; j++){
-            context.add.text(i * 100 + 50, j * 100 + 50, '[' + i + ';' + j + ']',{
-                fontSize       : '12px',
-                padding        : {
-                    x          : 10,
-                    y          : 5
-                },
-                fill           : '#ffffff'
-            })
-            .setOrigin(.5, .5)
-            .setDepth(.1);
+function initTiled(context){
+    const map = context.make.tilemap({
+        key: 'map'
+    });
+    const tileset = map.addTilesetImage('Tileset', 'tiles');
+
+    var ground_Layer = map.createLayer('Ground', tileset);
+    var wall_Layer = map.createLayer('Wall', tileset);
+}
+
+
+function initInputs(context){
+    cursors = context.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.Z,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.Q,
+        right: Phaser.Input.Keyboard.KeyCodes.D
+    });
+}
+
+
+function initPlayer(context){
+    shadow = context.add.image(750, 490, 'shadow')
+        .setOrigin(.5, 1)
+        .setDepth(1);
+    player = context.physics.add.sprite(750, 475, 'player')
+        .setOrigin(.5, 1)
+        .setDepth(1);
+    playerWalkspeed = 5;
+    playerIsMoving = false;
+    currentX = 0;
+    nextX = 0;
+    currentY = 0;
+    nextY = 0;
+    playerMoney = 150;
+    playerKey = 1;
+    playerLightning = 1;
+    playerFire = 1;
+    currentFloor = 0;
+}
+
+
+function initCamera(context){
+    context.cameras.main.startFollow(player);
+    context.cameras.main.setBounds(0, 0, player.widthInPixels, player.heightInPixels);
+}
+
+
+function printCases(context, departX, departY, arriveeX, arriveeY) {
+    for (i = departX; i < arriveeX + 1; i++) {
+        for (j = departY; j < arriveeY + 1; j++) {
+            context.add.text(i * 100 + 50, j * 100 + 50, '[' + i + ';' + j + ']', {
+                    fontSize: '12px',
+                    padding: {
+                        x: 10,
+                        y: 5
+                    },
+                    fill: '#ffffff'
+                })
+                .setOrigin(.5, .5)
+                .setDepth(.1);
         }
     }
-}           //Prints Cases' IDs from [departX;departY] to [arriveeX;arriveeY] (Xs et Ys being in-game Case Coordinates, not X and Y Coordinates)
+} //Prints Cases' IDs from [departX;departY] to [arriveeX;arriveeY] (Xs et Ys being in-game Case Coordinates, not X and Y Coordinates)
 
 
-function debugDisplay(config){
+function debugDisplay(config) {
     if (config.physics.arcade.debug) {
-        debugText.setText('gator is moving : ' + playerIsMoving + ' currentX : ' + currentX + ' nextX : ' + nextX + 
-        '\ngator current Case : [' + getCaseX(player.x) + ';' + getCaseY(player.y) + ']');
+        debugText.setText('gator is moving : ' + playerIsMoving + ' currentX : ' + currentX + ' nextX : ' + nextX +
+            '\ngator current Case : [' + getCaseX(player.x) + ';' + getCaseY(player.y) + ']');
     }
 
-}           //What's in the Update Event and that's about the custom debugger
+} //What's in the Update Event and that's about the custom debugger
 
 
-function upgradeUI(){
+function upgradeUI() {
     uiTextKey.setText(playerKey);
     uiTextMoney.setText(playerMoney);
     uiTextFire.setText(playerFire);
     uiTextLightning.setText(playerLightning);
 
-    if(currentFloor == 0)   uiTextStair.setText('G.F.');
-    else    uiTextStair.setText(currentFloor + 'F.');
-}           //What's in the Update Event and that's about UI
+    if (currentFloor == 0) uiTextStair.setText('G.F.');
+    else uiTextStair.setText(currentFloor + 'F.');
+} //What's in the Update Event and that's about UI
 
 
 function deplacementsPlayer() {
 
-    if (!playerIsMoving){
-        if (cursors.right.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.up.isDown) {
-            currentX       = player.x;
-            nextX          = player.x + 100;
+    if (!playerIsMoving) {
+        if (cursors.right.isDown) {
+            currentX = player.x;
+            nextX = player.x + 100;
             playerIsMoving = true;
-        } else if (cursors.left.isDown && !cursors.right.isDown && !cursors.down.isDown && !cursors.up.isDown) {
-            currentX       = player.x;
-            nextX          = player.x - 100;
+        } else if (cursors.left.isDown) {
+            currentX = player.x;
+            nextX = player.x - 100;
             playerIsMoving = true;
-        } else if (cursors.down.isDown && !cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown) {
-            currentY       = player.y;
-            nextY          = player.y + 100;
+        } else if (cursors.down.isDown) {
+            currentY = player.y;
+            nextY = player.y + 100;
             playerIsMoving = true;
-        } else if (cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.right.isDown) {
-            currentY       = player.y;
-            nextY          = player.y - 100;
+        } else if (cursors.up.isDown) {
+            currentY = player.y;
+            nextY = player.y - 100;
             playerIsMoving = true;
         }
     }
@@ -342,6 +358,7 @@ function deplacementsPlayer() {
 
 
 function playerMoves() {
+
     if (playerIsMoving) {
 
         if (currentX <= nextX && currentX != 0) {
@@ -395,19 +412,21 @@ function playerMoves() {
 }
 
 
-function getCaseX(value){
-    return (value-50)/100;
+function getCaseX(value) {
+    return (value - 50) / 100;
 }
 
 
-function getCaseY(value){
-    return (value-75)/100;
+function getCaseY(value) {
+    return (value - 75) / 100;
 }
 
-function caseXToCoord(value){
+
+function caseXToCoord(value) {
     return value * 100 + 50;
 }
 
-function caseYToCoord(value){
+
+function caseYToCoord(value) {
     return value * 100 + 75;
 }
